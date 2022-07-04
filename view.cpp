9205a5451bacf491f816b6a3f66483ff9726a735
Przemek Badaruk <przemekkth@outlook.com>
2022-07-04 08:50:55 +0200

    refactor

--- view.cpp
+++ view.cpp
@@ -1,4 +1,6 @@
 #include "view.h"
+#include <QKeyEvent>
+#include <QApplication>
 
 View::View()
     : m_gameScene(new GameScene(this))
@@ -7,4 +9,17 @@ View::View()
     resize(m_gameScene->sceneRect().width() + 2, m_gameScene->sceneRect().height() + 2);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+}
+
+void View::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+        case Qt::Key_Escape:
+        {
+            QApplication::instance()->quit();
+        }
+        break;
+    }
+    QGraphicsView::keyPressEvent(event);
 }
